76faf7560d1904e2b330e5c7393d9687b338875a
woksin <sindrewilting@gmail.com>
2026-09-25 03:56:54 +0200

    Clarify dual-mode event appends and cover inferred owners

--- Source/Chronicle/for_ChronicleArtifacts/when_registering/with_inferred_read_models.ts
+++ Source/Chronicle/for_ChronicleArtifacts/when_registering/with_inferred_read_models.ts
@@ -1,7 +1,8 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import { field } from '@cratis/fundamentals';
-import { fromEvent, setFrom } from '@cratis/chronicle/projections';
+import { fromEvent, setFrom, projection } from '@cratis/chronicle/projections';
+import { reducer } from '@cratis/chronicle/reducers';
 import { eventType } from '@cratis/chronicle/events';
 import { ChronicleArtifacts } from '../../ChronicleArtifacts.js';
 import { given } from '@cratis/arc.testing';
@@ -11,6 +12,12 @@ class Registered { @field(String) name = ''; }
 @fromEvent(Registered)
 class FromEventModel { @field(String) name = ''; }
 class PropertyModel { @field(String) @setFrom(Registered) name = ''; }
+class ProjectionModel { @field(String) name = ''; }
+class ReducerModel { @field(String) name = ''; }
+@projection('ArcInferredProjection', ProjectionModel)
+class ProjectNames {}
+@reducer('ArcInferredReducer', undefined, ReducerModel)
+class ReduceNames {}
 class Unrelated { @field(String) name = ''; }
 
 class a_catalog { readonly catalog = new ChronicleArtifacts(); }
@@ -19,10 +26,15 @@ describe('when registering inferred read models', given(a_catalog, context => {
     beforeEach(() => {
         context.catalog.register(FromEventModel);
         context.catalog.register(PropertyModel);
+        context.catalog.register(ProjectNames);
+        context.catalog.register(ReduceNames);
         context.catalog.register(Unrelated);
     });
     it('should include model-bound read models without the deprecated Chronicle decorator', () => {
         context.catalog.readModels.should.include(FromEventModel).and.include(PropertyModel);
+    });
+    it('should infer read models owned by registered projections and reducers', () => {
+        context.catalog.readModels.should.include(ProjectionModel).and.include(ReducerModel);
     });
     it('should not register unrelated types', () => {
         context.catalog.readModels.should.not.include(Unrelated);
